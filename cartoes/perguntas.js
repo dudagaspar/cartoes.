adb94380ff9db5b4455324927757cb06c60e0b58
dudagaspar <00001088697720sp@al.educacao.sp.gov.br>
2024-11-23 01:30:34 -0300

    perguntas.js

--- cartoes/perguntas.js
+++ cartoes/perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é um loop em programação e qual a sua utilidade?',
+    'Um loop (ou laço de repetição) em programação é uma estrutura que permite executar um bloco de código várias vezes, de acordo com uma condição específica. Ele é muito útil para automatizar tarefas repetitivas, como processar uma lista de dados ou realizar cálculos até que uma condição seja atendida. Existem diferentes tipos de loops, como o for loop (que repete um bloco de código um número fixo de vezes) e o while loop (que repete enquanto uma condição for verdadeira).'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a principal diferença entre clima e tempo?',
+    'A principal diferença entre clima e tempo está no período de observação: Tempo refere-se às condições atmosféricas em um local específico e em um curto período de tempo, como horas ou dias. Por exemplo, "Hoje está chovendo e a temperatura está em 20°C." Clima, por outro lado, é a média das condições atmosféricas de uma região ao longo de um longo período de tempo, geralmente 30 anos ou mais. Ele leva em conta variáveis como temperatura, precipitação, umidade, entre outros. Por exemplo, "O clima de uma região tropical é quente e úmido durante a maior parte do ano." Portanto, enquanto o tempo descreve o estado atual da atmosfera, o clima descreve padrões a longo prazo.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'Quanto é 7+5?',
+    'A soma de 7 + 5 é igual a 12.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz borboleta em Inglês?',
+    'A palavra "borboleta" em inglês é butterfly.'
 )
